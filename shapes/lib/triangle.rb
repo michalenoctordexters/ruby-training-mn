fa07407e0531c62892e37fcfa127ec170d61dbff
noctorious-98 <michael.noctor@gmail.com>
2026-05-14 15:00:39 +0000

    traingle.rb answers

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError if [a, b, c].any? { |side| side <= 0 }
+  s1, s2, s3 = [a, b, c].sort
+  raise TriangleError if s1 + s2 <= s3
+  if a == b && b == c
+    :equilateral
+  elsif a == b || b == c || a == c
+    :isosceles
+  else
+    :scalene
+  end
 end
+
+
 
 # Error class used in triangle_spec. No need to change this code.
 class TriangleError < StandardError
