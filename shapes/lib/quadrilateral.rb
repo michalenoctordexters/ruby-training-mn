8f880c6265082d4f88c88fe3b6c35f45d225fecb
noctorious-98 <michael.noctor@gmail.com>
2026-05-15 14:50:04 +0000

    quadlirateral.rb answers

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 def quadrilateral(a, b, c, d)
-  # WRITE THIS CODE
+  raise QuadrilateralError if a <= 0 || b <= 0 || c <= 0 || d <= 0
+  raise QuadrilateralError if [a, b, c, d].sum != 360
+  if a == b && b == c && c == d
+    [:square, :rectangle]
+  elsif (a == b && c == d) || (a == c && b == d) || (a == d && b == c)
+    [:parallelogram, :rhombus]
+    else [:quadrilateral]
+  end
 end
 
 # Error class used in quadrilateral_spec. No need to change this code.
