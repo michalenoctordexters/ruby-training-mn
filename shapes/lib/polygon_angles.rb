a4853276ce42809eedf42f94dd664dbd271e3b50
noctorious-98 <michael.noctor@gmail.com>
2026-05-15 15:11:47 +0000

    polygon_angles.rb answers

--- shapes/lib/polygon_angles.rb
+++ shapes/lib/polygon_angles.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 def internal_angle(number_of_sides)
-  # WRITE THIS CODE
+  ((number_of_sides - 2) * 180.0 / number_of_sides).round(2)
 end
 
 def external_angle(number_of_sides)
-  # WRITE THIS CODE
+  (360.0 / number_of_sides).round(2)
 end
